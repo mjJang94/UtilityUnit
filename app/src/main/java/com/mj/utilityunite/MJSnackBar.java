85442c19d24c9f0cdc371ef6e002be4eea8f34b8
PayMint_MJ <alswhddl10@naver.com>
2019-11-15 13:24:22 +0900

    전역변수로 선언하지 않은 부분 있어서 다시 커밋

--- app/src/main/java/com/mj/utilityunite/MJSnackBar.java
+++ app/src/main/java/com/mj/utilityunite/MJSnackBar.java
@@ -6,11 +6,11 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class MJSnackBar {
 
-    public void makeShortSnackBar(View view, String message){
+    public static void makeShortSnackBar(View view, String message){
         Snackbar.make(view, message, Snackbar.LENGTH_SHORT).show();
     }
 
-    public void makeLongSnackBar(View view, String message){
+    public static void makeLongSnackBar(View view, String message){
         Snackbar.make(view, message, Snackbar.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/mj/utilityunite/MJToast.java
+++ app/src/main/java/com/mj/utilityunite/MJToast.java
@@ -5,11 +5,11 @@ import android.widget.Toast;
 
 public class MJToast {
 
-    public void makeShortToast(Context context, String message){
+    public static void makeShortToast(Context context, String message){
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
 
-    public void makeLongToast(Context context, String message){
+    public static void makeLongToast(Context context, String message){
         Toast.makeText(context, message, Toast.LENGTH_LONG).show();
     }
 }
