7b427b3c9f8a77079899da436d09699c57d3766c
PayMint_MJ <alswhddl10@naver.com>
2020-02-12 11:35:04 +0900

    풀을 위한 커밋

--- app/src/main/java/com/mj/utilityunite/MJSnackBar.java
+++ app/src/main/java/com/mj/utilityunite/MJSnackBar.java
@@ -1,5 +1,6 @@
 package com.mj.utilityunite;
 
+import android.content.Context;
 import android.view.View;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -9,7 +10,6 @@ public class MJSnackBar {
     public static void makeShortSnackBar(View view, String message){
         Snackbar.make(view, message, Snackbar.LENGTH_SHORT).show();
     }
-
     public static void makeLongSnackBar(View view, String message){
         Snackbar.make(view, message, Snackbar.LENGTH_LONG).show();
     }
